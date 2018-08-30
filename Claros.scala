b738d719c14055b3273cf1883c9065c13f959ea4
jihanejabri <35765837+jihanejabri@users.noreply.github.com>
2018-08-30 15:55:49 +0200

    Add files via upload

--- Claros.scala
+++ Claros.scala
@@ -4,27 +4,35 @@ object  example extends App {
 
   val trip=sc.textFile("C:/Users/Jabri/Desktop/RDF/Claros.csv").map(x=>x.split(" "))
   val.count
+  val ClarosE=sc.textFile("C:/Users/Jabri/Desktop/RDF/Claros_L+eqr.dlog.txt").map(x=>x.split(" ")).map(x=>(x(1),(x(0),x(2))))
+  //1343
+  ClarosEqr.count
+  val ClarosEq=sc.textFile("C:/Users/Jabri/Desktop/RDF/Claros_L+eq.dlog.txt").map(x=>x.split(" "))//1343
+  ClarosEq.count
 
-  def lireTriples(input: String): RDD[(Long, Long, Long, Int)] = {
+  val trip2 = ClarosEqr.filter(x=>x.length()>0)
+  trip2.count//1342
+
+
+  def lireT(input: String): RDD[(Long, Long, Int)] = {
     return sc.textFile(input).
-      map(line => line.substring(1, line.length -1).split(",")).
-      map(tab => (tab(0).toLong, tab(1).toLong, tab(2).toLong, (tab(0).toInt)
+      map(l => l.substring(1, line.length -1).split(",")).
+      map(t => (t(0).toLong, t(1).toLong, (t(0).toInt)))
   }
 
-    val triples = lireTriples(trip)
-    triples.persist
+    val triples = lireT(trip)
+    triples.persist//31
 
     triples.count
 
-  def filterProp(input: RDD[(Long, Long, Long, Int)], prop: Long): RDD[(Long, Long, Int)] = {
-    x  // on projette sur S et O car P est fixe
+  def filterPropreties(input: RDD[(Long, Long, Int)], prop: Long): RDD[(Long, Long, Int)] = {
+    x
     return input.filter(x => x._2 == prop).map{ case (s,p,o,f) => (s,o,f)}
   }
 
-  val trip2 = trip.filter(x=>x.length()>0)
-  trip2.count
 
-  val l = trip2.map(x=>x.length()).reduce((a,b)=>a.max(b))
+
+  val l = trip2.map(x=>x.length()).reduce((a,b)=>a.max(b))//145
 
   val t = trip2.map(x=>x.length()).reduce((a,b)=>a.min(b))
 
