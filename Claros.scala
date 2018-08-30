f149b6ff2083bd7b6d0322eb0406f36841fc7629
jihanejabri <35765837+jihanejabri@users.noreply.github.com>
2018-08-30 17:50:29 +0200

    Update Claros.scala

--- Claros.scala
+++ Claros.scala
@@ -3,14 +3,14 @@ package Claros
 object  example extends App {
 
   val trip=sc.textFile("C:/Users/Jabri/Desktop/RDF/Claros.csv").map(x=>x.split(" "))
-  val.count
+  val.count//37
   val ClarosE=sc.textFile("C:/Users/Jabri/Desktop/RDF/Claros_L+eqr.dlog.txt").map(x=>x.split(" ")).map(x=>(x(1),(x(0),x(2))))
   //1343
-  ClarosEqr.count
+  ClarosEq.count
   val ClarosEq=sc.textFile("C:/Users/Jabri/Desktop/RDF/Claros_L+eq.dlog.txt").map(x=>x.split(" "))//1343
   ClarosEq.count
 
-  val trip2 = ClarosEqr.filter(x=>x.length()>0)
+  val trip2 = ClarosEq.filter(x=>x.length()>0)
   trip2.count//1342
 
 
@@ -20,22 +20,29 @@ object  example extends App {
       map(t => (t(0).toLong, t(1).toLong, (t(0).toInt)))
   }
 
-    val triples = lireT(trip)
-    triples.persist//31
+    val triplesE = lireT(ClarosE)
+    triplesE.persist//31
 
-    triples.count
+    triplesE.count
+  
+    val triplesEq = lireT(ClarosEq)
+    triplesEq.persist//31
+
+    triplesEq.count
 
   def filterPropreties(file: RDD[(Long, Long, Int)], prop: Long): RDD[(Long, Long, Int)] = {
     return file.filter(x => x._2 == prop).map{ case (s,p,o,f) => (s,o,f)}
   }
 
-  val TripOwl = filterProperties(triples)
-  
+  val TripOwl1 = filterProperties(triplesE)//70
+  val TripOwl2 = filterProperties(triplesEq)//82
   
 
-  val l = trip2.map(x=>x.length()).reduce((a,b)=>a.max(b))//145
-
-  val t = trip2.map(x=>x.length()).reduce((a,b)=>a.min(b))
+  val l = TripOwl1.map(x=>x.length()).reduce((a,b)=>a.max(b))
+  val t = TripOwl1.map(x=>x.length()).reduce((a,b)=>a.min(b))
+  
+  val l = TripOwl2.map(x=>x.length()).reduce((a,b)=>a.max(b))
+  val t = TripOwl2.map(x=>x.length()).reduce((a,b)=>a.min(b))
 
 
 }
